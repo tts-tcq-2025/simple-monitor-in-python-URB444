f5d4a5aad1ad93ca7a4d5f6b2d26c4aaf3696d32
URB444 <87970004+URB444@users.noreply.github.com>
2025-07-21 10:07:41 +0530

    Update check_limits.py
    Added a check_limits method to reduce cyclometic complexity

--- check_limits.py
+++ check_limits.py
@@ -1,15 +1,13 @@
-def battery_is_ok(temperature, soc, charge_rate):
-    limits = {
-        'Temperature': (temperature, 0, 45),
-        'State of Charge': (soc, 20, 80),
-        'Charge rate': (charge_rate, 0, 0.8)
-    }
-    for name, (value, low, high) in limits.items():
-        if value < low or value > high:
-            print(f'{name} is out of range!')
-            return False
+def check_limits(val, low_limit, high_limit):
+    if val < low_limit or val > high_limit:
+        return False
     return True
 
+def battery_is_ok(temperature, soc, charge_rate):
+    return (check_limits(temperature,0,45,'Temperature') and
+            check_limits(soc,0,45,'State of Charge') and
+            check_limits(charge_rate,0,0.8,'Charge rate')
+           )
 if __name__ == '__main__':
     assert(battery_is_ok(5, 90, 0.7) is False)
     assert(battery_is_ok(50, 85, 0) is False)
